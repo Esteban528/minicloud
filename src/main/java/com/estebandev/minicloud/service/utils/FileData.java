58d61b60b0ffe01ad547f894e68e228ae03f0350
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-02-10 16:17:59 -0500

    feat(test): add unit tests

--- src/main/java/com/estebandev/minicloud/service/utils/FileData.java
+++ src/main/java/com/estebandev/minicloud/service/utils/FileData.java
@@ -2,11 +2,15 @@ package com.estebandev.minicloud.service.utils;
 
 import java.nio.file.Path;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FileData {
     String fileName;
     Path path;
