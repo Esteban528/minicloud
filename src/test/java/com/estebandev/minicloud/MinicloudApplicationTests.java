a38abc4262fe7bd7721ef06734ae56c216eaf98b
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-03-21 22:37:05 -0500

    test: add CustomErrorController mock bean

--- src/test/java/com/estebandev/minicloud/MinicloudApplicationTests.java
+++ src/test/java/com/estebandev/minicloud/MinicloudApplicationTests.java
@@ -2,9 +2,14 @@ package com.estebandev.minicloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.estebandev.minicloud.config.CustomErrorController;
 
 @SpringBootTest
 class MinicloudApplicationTests {
+    @MockitoBean
+    CustomErrorController customErrorController;
 
 	@Test
 	void contextLoads() {
