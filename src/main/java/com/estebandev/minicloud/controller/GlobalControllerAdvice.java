18cb8e5c790d4603676e307626a9f3a1dc367af8
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-03-25 21:48:50 -0500

    feat: add title to frontend

--- src/main/java/com/estebandev/minicloud/controller/GlobalControllerAdvice.java
+++ src/main/java/com/estebandev/minicloud/controller/GlobalControllerAdvice.java
@@ -2,6 +2,7 @@ package com.estebandev.minicloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -20,9 +21,16 @@ public class GlobalControllerAdvice {
 
     @ModelAttribute
     public void addModelAttributes(Model model, HttpServletRequest request) {
+        String mainEndpoint = "";
+        String[] requestArgs = request.getRequestURI().split("/");
 
-        model.addAttribute("title", title);
+        if (requestArgs.length > 0) {
+            mainEndpoint = requestArgs[1];
+        }
+
+        model.addAttribute("title", StringUtils.capitalize(title));
         model.addAttribute("request", request);
         model.addAttribute("auth", userService.isAuthenticated());
+        model.addAttribute("mainEndpoint", StringUtils.capitalize(mainEndpoint));
     }
 }
